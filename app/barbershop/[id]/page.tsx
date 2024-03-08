05f0f1e50e3b16d0fc5a16da4df7641807b4bd39
paulo9920 <paulovitorsantosvianna@gmail.com>
2024-03-08 14:33:25 -0300

    feat: add barbershop info component

---
+++ app/barbershop/[id]/page.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import BarbershopInfo from "./_components/barbeshop-info";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+  if (!params.id) {
+    // TODO: redirecionar para home page
+    return null;
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    }
+  });
+
+  if (!barbershop) {
+    // TODO: redirecionar para home page
+    return null;
+  }
+
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+    </div>
+  );
+};
+
+export default BarbershopDetailsPage;
