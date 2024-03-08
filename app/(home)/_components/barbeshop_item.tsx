975005187641fefdafe3eb6a44fbd564121b53e1
paulo9920 <paulovitorsantosvianna@gmail.com>
2024-03-08 14:30:12 -0300

    feat: add barbershop info component

--- app/(home)/_components/barbeshop_item.tsx
+++ app/(home)/_components/barbeshop_item.tsx
@@ -1,34 +1,55 @@
+"use client";
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
-import   Image   from "next/image";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-interface BarbeshopItemProps {
-    barbershop: Barbershop;
+interface BarbershopItemProps {
+  barbershop: Barbershop;
 }
 
-const BarbershopItem = ({barbershop}: BarbeshopItemProps) => {
-    return ( 
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  const router = useRouter();
 
-        <Card className="min-w-[167px] max-w-[167] rounded-2xl">
-            <CardContent className="px-1 py-0">
-            <div className="px-1 relative w-full h-[159px]">
-                <Badge variant="secondary" className=" opacity-90 items-center absolute top-2 left-2 z-50">
-                    <StarIcon size={12} className="fill-primary text-primary"/>
-                    <span className="text-xs">5,0</span>
-                </Badge>
-                <Image alt={barbershop.name} src={barbershop.imageUrl} style={{Objectfit: "cover"}} fill className="rounded-2xl"/>
-            </div>
-            <div className="px-2 pb-0">
-                <h2 className="fount-bold mt-2  overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
-                <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
-                <Button className="w-full mt-3" variant="secondary">Reservar</Button>
-            </div>
-            </CardContent>
-        </Card>
-     );
-}
- 
+  const handleBookingClick = () => {
+    router.push(`/barbershop/${barbershop.id}`);
+  };
+
+  return (
+    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+      <CardContent className="px-1 py-0">
+        <div className="w-full h-[159px] relative">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
+          <Image
+            alt={barbershop.name}
+            src={barbershop.imageUrl}
+            style={{
+              objectFit: "cover",
+            }}
+            fill
+            className="rounded-2xl"
+          />
+        </div>
+
+        <div className="px-2 pb-3">
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
+          <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
 export default BarbershopItem;
