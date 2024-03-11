6854bd1f5e2727ce5a12360079f9193fa15314f4
paulo9920 <paulovitorsantosvianna@gmail.com>
2024-03-11 10:40:49 -0300

    feat: add sig in and sign out

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,33 +1,86 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import { MenuIcon } from "lucide-react";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon, MenuIcon, UserIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
-const Header = () =>{
-    const {data} = useSession();
-    const handleLoginClick = async () =>{
-        await signIn();
-    }
-    return(
-        <Card>
-            <CardContent className="p-5 justify-between items-center flex flex-row">
-            <Image src="/Logo.png" alt="FSW barber" height={22} width={120}/>
-            {/* <Button variant="outline" size="icon" className="h-6 w-6">
-                <MenuIcon size={18}/>
-            </Button> */}
-            {data?.user ?(
-                <div>
-                    <Button onClick={() =>signOut()}>Logout</Button>
-                    <h1>{data.user.name}</h1>
+const Header = () => {
+  const { data } = useSession();
+
+  const handleLogoutClick = () => signOut();
+
+  const handleLoginClick = () => signIn("google");
+
+  return (
+    <Card>
+      <CardContent className="p-5 justify-between items-center flex flex-row">
+        <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SheetHeader className="text-left border-b border-solid border-secondary p-5">
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+
+            {data?.user ? (
+              <div className="flex justify-between px-5 py-6 items-center">
+                <div className="flex items-center gap-3">
+                  <Avatar>
+                    <AvatarImage src={data.user?.image ?? ""} />
+                  </Avatar>
+
+                  <h2 className="font-bold">{data.user.name}</h2>
                 </div>
-            ):(
-                <Button onClick={handleLoginClick}>Login</Button>
+
+                <Button variant="secondary" size="icon">
+                  <LogOutIcon onClick={handleLogoutClick} />
+                </Button>
+              </div>
+            ) : (
+              <div className="flex flex-col px-5 py-6 gap-3">
+                <div className="flex items-center gap-2">
+                  <UserIcon size={32} />
+                  <h2 className="font-bold">Olá, faça seu login!</h2>
+                </div>
+                <Button variant="secondary" className="w-full justify-start" onClick={handleLoginClick}>
+                  <LogInIcon className="mr-2" size={18} />
+                  Fazer Login
+                </Button>
+              </div>
             )}
-            </CardContent>
-        </Card>
-    );
-}
+
+            <div className="flex flex-col gap-3 px-5">
+              <Button variant="outline" className="justify-start" asChild>
+                <Link href="/">
+                  <HomeIcon size={18} className="mr-2" />
+                  Início
+                </Link>
+              </Button>
+
+              {data?.user && (
+                <Button variant="outline" className="justify-start" asChild>
+                  <Link href="/bookings">
+                    <CalendarIcon size={18} className="mr-2" />
+                    Agendamentos
+                  </Link>
+                </Button>
+              )}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </CardContent>
+    </Card>
+  );
+};
+
 export default Header;
